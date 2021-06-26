364dd5e80e3bd668a220a3b2a6b76478f24fb647
Nicat <nicatnagizade53@gmail.com>
2021-06-26 20:07:48 +0200

    created admin context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes'
+import ContextProvider from './contexts';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <ContextProvider>
         <Routes />
+      </ContextProvider>
     </BrowserRouter>
   );
 }
