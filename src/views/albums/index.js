8f840a303b5be737e7ed1823f0a0403af2f61251
Nicat <nicatnagizade53@gmail.com>
2021-06-27 18:40:36 +0200

    updated text on album view

--- src/views/albums/index.js
+++ src/views/albums/index.js
@@ -32,7 +32,7 @@ export default function Albums() {
                                     </div>
                                     <button className="read-more">
                                         <Link to={`/album/${album.id}/photo`}>
-                                            READ MORE
+                                            SHOW ALL
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" /></svg>
                                         </Link>
                                     </button>
