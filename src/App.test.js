f53c776ace48db55c836186964f99d3e64f28435
Nicat <nicatnagizade53@gmail.com>
2021-07-01 03:33:23 +0200

    created tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,71 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { cleanup, render, screen } from '@testing-library/react';
+import Albums from './views/albums'
+import Photos from './views/photos'
+import { createdAlbumContext } from './contexts/album'
+import { BrowserRouter } from 'react-router-dom';
+import Header from './layouts/header'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+afterEach(cleanup)
+
+const albumContext = {
+  albums : {
+    loading: false, isLoaded: true,
+    data: [
+      {
+        id: 1,
+        userId: 1,
+        title: "test title",
+        photos: [
+          {
+            albumId: 1,
+            title: "photo test title",
+            url: "https://via.placeholder.com/600/92c952",
+            thumbnailUrl: "https://via.placeholder.com/150/92c952"
+          }
+        ]
+      }
+    ]
+  },
+  getIfIsNotLoaded : () => { }
+}
+
+test('Testing home link on header', () => {
+  render(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  )
+  expect(screen.getByTestId('home-link')).toHaveAttribute('href', '/')
+})
+
+test('Testing albums view', () => {
+  const {albums, getIfIsNotLoaded} = albumContext
+  render(
+    <BrowserRouter>
+      <createdAlbumContext.Provider value={{ albums, getIfIsNotLoaded }}>
+        <Albums />
+      </createdAlbumContext.Provider>
+    </BrowserRouter>
+  );
+  const album = albums.data[0]
+  expect(screen.getByTestId('albums')).toHaveTextContent(album.title)
+  const showAllButton = screen.getByTestId('albums').querySelector('.read-more')
+  expect(showAllButton.querySelector('a')).toHaveAttribute('href', '/album/'+album.id+'/photo')
+  const photoComponent = screen.getByTestId('albums').querySelector('.card')
+  expect(photoComponent.querySelector('img')).toHaveAttribute('src', album.photos[0].thumbnailUrl)
+})
+
+test('Testing photos view', () => {
+  const {albums, getIfIsNotLoaded} = albumContext
+  const album = albums.data[0]
+  const photo = album.photos[0]
+  render(
+    <BrowserRouter>
+      <createdAlbumContext.Provider value={{ albums, getIfIsNotLoaded }}>
+        <Photos match={{params:{id:1}}} />
+      </createdAlbumContext.Provider>
+    </BrowserRouter>
+  );
+  expect(screen.getByTestId('photos')).toHaveTextContent(photo.title)
+  expect(screen.getByTestId('photos').querySelector('img')).toHaveAttribute('src', photo.url)
+})
--- src/contexts/album/index.js
+++ src/contexts/album/index.js
@@ -2,10 +2,9 @@ import React, { createContext, useContext, useState } from 'react'
 import axios from 'axios'
 import { API_URL } from '../../config';
 
-const ContextAlbum = createContext();
-const initialAlbums = { loading: true, isLoaded: false, errors: [], data: [] }
-
-export const AlbumContext = () => useContext(ContextAlbum);
+export const createdAlbumContext = createContext();
+export const AlbumContext = () => useContext(createdAlbumContext);
+const initialAlbums = { loading: true, isLoaded: false, failed: false, errors: [], data: [] }
 
 export default function AlbumProvider(props) {
     const [albums, setAlbums] = useState(initialAlbums)
@@ -18,6 +17,9 @@ export default function AlbumProvider(props) {
             .then(res => {
                 setAlbums({ ...albums, loading: false, isLoaded: true, data: res.data })
             })
+            .catch((err) => {
+                setAlbums({...albums, loading: false, isLoaded: true, failed: true, errors:err.response.data})
+            })
     }
     const getIfIsNotLoaded = () => {
         if (!albums.isLoaded) {
@@ -25,12 +27,12 @@ export default function AlbumProvider(props) {
         }
     }
     return (
-        <ContextAlbum.Provider value={{
+        <createdAlbumContext.Provider value={{
             albums, setAlbums,
             getAlbums,
             getIfIsNotLoaded
         }}>
             {props.children}
-        </ContextAlbum.Provider>
+        </createdAlbumContext.Provider>
     )
 }
